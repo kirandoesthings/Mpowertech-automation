740a6779533d719b7e2ca2384daa4d07b1477440
kirandoesthings <kandulapatibunny@gmail.com>
2026-04-02 10:49:16 +0530

    restructure folders, add CI pipelines, fix credential security

--- pages/admin/login_page_pw.py
+++ pages/admin/login_page_pw.py
@@ -1,13 +1,19 @@
-class LoginPagePW:
-    URL = "https://test.mstart.co.in/login"  # Note: redirects from /admin/login
-    
-    def __init__(self, page):
-        self.page = page
-    
-    def login(self, email="shivakumarkyamaji@gmail.com", password="Shiva@123"):
-        self.page.goto(self.URL)
-        self.page.fill("#email", email)
-        self.page.fill("#password", password)
-        self.page.click("button[type='submit']")
-        # Wait for login to complete
-        self.page.wait_for_url("**/admin/**")
+import os 
+from dotenv import load_dotenv 
+  
+load_dotenv() 
+  
+class LoginPagePW: 
+    URL = "https://test.mstart.co.in/login" 
+      
+    def __init__(self, page): 
+        self.page = page 
+      
+    def login(self, email=None, password=None): 
+        email = email or os.environ.get("MUDU_USERNAME") 
+        password = password or os.environ.get("MUDU_PASSWORD") 
+        self.page.goto(self.URL) 
+        self.page.fill("#email", email) 
+        self.page.fill("#password", password) 
+        self.page.click("button[type='submit']") 
+        self.page.wait_for_url("**/admin/**") 
